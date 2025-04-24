47e263a24b9d95a662a0c78f2a5d33f234262271
Ivan Gonzalo Berrutto <ivanberrutto@gmail.com>
2025-04-24 00:58:41 -0300

    corrijo nodo size

--- ASM/src/3 - Alineación y Estructuras/structs.h
+++ ASM/src/3 - Alineación y Estructuras/structs.h
@@ -18,15 +18,15 @@ typedef struct __attribute__((__packed__)) packed_nodo_s {
     struct packed_nodo_s* next;   //asmdef_offset:PACKED_NODO_OFFSET_NEXT 8 0
     uint8_t categoria;     //asmdef_offset:PACKED_NODO_OFFSET_CATEGORIA 1 8
     uint32_t* arreglo;     //asmdef_offset:PACKED_NODO_OFFSET_ARREGLO 8 9
-    uint32_t longitud;     //asmdef_offset:PACKED_NODO_OFFSET_LONGITUD 8 17
+    uint32_t longitud;     //asmdef_offset:PACKED_NODO_OFFSET_LONGITUD 4 17
 } packed_nodo_t; //asmdef_size:PACKED_NODO_SIZE 21
 
 // Puntero al primer nodo que encabeza la lista
 typedef struct lista_s {
-    nodo_t* head;    //asmdef_offset:LISTA_OFFSET_HEAD
-} lista_t; //asmdef_size:LISTA_SIZE
+    nodo_t* head;    //asmdef_offset:LISTA_OFFSET_HEAD 8 0
+} lista_t; //asmdef_size:LISTA_SIZE 8
 
 // Puntero al primer nodo que encabeza la lista
 typedef struct __attribute__((__packed__)) packed_lista_s {
-    packed_nodo_t* head;    //asmdef_offset:PACKED_LISTA_OFFSET_HEAD
-} packed_lista_t; //asmdef_size:PACKED_LISTA_SIZE
+    packed_nodo_t* head;    //asmdef_offset:PACKED_LISTA_OFFSET_HEAD 8 0
+} packed_lista_t; //asmdef_size:PACKED_LISTA_SIZE 8
